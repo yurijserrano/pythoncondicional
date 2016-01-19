4f63ee5cd55fc0ef5b849b9f945f606351846dca
Yuri <yurijserrano@gmail.com>
2016-01-19 10:10:13 -0200

    Primeiro Programa condicional

---
+++ programa1.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+
+
+x = 1
+y = 1
+variavel = 4
+nome_jogador = 'Bob'
+
+
+if x == y:
+	print "x é igual a y"
+if  variavel > 2:
+	print "A variável é maior que 2"
+if nome_jogador == "Bob":
+	print "O nome do jogador é Bob"
