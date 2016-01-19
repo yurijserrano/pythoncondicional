d54c8370c81ce9e435f2f665a3d69ddbfff78e94
Yuri <yurijserrano@gmail.com>
2016-01-19 11:49:45 -0200

    Quinto Programa Condicional

---
+++ programa5.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+
+"""
+
+Utilização do comando not in
+
+Ele é utilizado para negar o valor booleano avaliado
+
+
+"""
+
+
+if 'bob' not in 'ab':
+	print "not in funcionou"
