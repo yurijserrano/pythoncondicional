57eb71a285c495341c38694f1f9bae2e81194934
Yuri <yurijserrano@gmail.com>
2016-01-19 11:36:02 -0200

    Quarto Programa Comdicional

---
+++ programa4.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+
+"""
+Utilização do not in 
+
+
+Ele é utilizado para negar o valor booleano de uma string 
+em comparação com outra.
+
+"""
+
+
+if not 'bob' in 'ab':
+	print "Embora bob não esteja em ab not troca a condição booleana"
+
+
+
